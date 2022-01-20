8a7478e042cbf01de2e078695a5b2f779ea8d543
hari-ls <hari@logicsync.com.au>
2022-01-20 17:42:53 +1030

    added pseudo code to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+// initiliase data
+
+// add variables and operators
+
+// create element & selectors
+
+// listen to search query event
+
+// fetch the data from the API
+
+// if successful format the data from the API
+
+// add the city to the list of suggested cities
+
+// update the data on the document
